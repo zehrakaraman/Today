7ca66c8cc94d0fe6f4e7e6533796221010f7ed68
Zehra Karaman <zehrakaramaan@gmail.com>
2022-12-04 20:57:21 +0300

    Fix the display of cell

---
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -0,0 +1,24 @@
+//
+//  ReminderListViewController+DataSource.swift
+//  Today
+//
+//  Created by Zehra on 4.12.2022.
+//
+
+import Foundation
+import UIKit
+
+extension ReminderListViewController {
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+    }
+}
