93b921d5214a7494d17981e6c27bef5d309d05b5
Zehra Karaman <zehrakaramaan@gmail.com>
2022-12-19 23:12:12 +0300

    Fix the content view

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Zehra on 19.12.2022.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func update(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
